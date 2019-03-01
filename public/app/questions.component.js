275df88eb385508eef269bb2f3e02a4b01184ba0
Matthew Mroue <mamroue@yahoo.com>
2019-03-01 15:28:21 -0500

    randomize 5 questions

--- public/app/questions.component.js
+++ public/app/questions.component.js
@@ -1,5 +1,7 @@
 "use strict";
 
+// import { isMainThread } from "worker_threads";
+
 const questions = {
     templateUrl:"app/questions.html",
     controller: ["QuizService", function(QuizService) {
@@ -7,6 +9,14 @@ const questions = {
 
         QuizService.getQuestions().then(response => {
             vm.questions = response.data;
+            // vm.questions = ["und", "und", "und", "und", "und"];
+
+            // console.log(vm.question);
+
+            // for(let i = 0; i < vm.questions.length; i++){
+            //     vm.questions[i] = vm.question[Math.floor(Math.random()*5)];
+            // }
+
             console.log(vm.questions);
         });
         vm.total = 0;
--- questions.routes.js
+++ questions.routes.js
@@ -4,7 +4,7 @@ const questions = express.Router();
 const pool = require("./connection")
 
 questions.get("/questions", function(req, res) {
-    pool.query("SELECT * FROM questionlist").then(function(response) {
+    pool.query("SELECT * FROM questionlist order by random() limit 5").then(function(response) {
         console.log(response)
         res.send(response.rows)
     });
